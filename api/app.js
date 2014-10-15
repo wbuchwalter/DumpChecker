1ba6e45abba5abfe19279cbaa9ab253ff9006dab
Will <wbuchwalter@gmail.com>
2014-10-15 17:54:53 -0400

    Structure api

---
+++ api/app.js
@@ -0,0 +1,15 @@
+{
+  "name": "DumpCheckerApi",
+  "version": "0.0.0",
+  "description": "API for DumpChecker",
+  "main": "app.js",
+  "author": {
+    "name": "William Buchwalter",
+    "email": "wbuchwalter@gmail.com"
+  },
+  "dependencies": {
+    "express": "3.4.4",
+    "jade": "*",
+    "stylus": "*"
+  }
+}
