b0b59e934978b78f3cdadb4fd3ac0efab1220e5a
Will <wbuchwalter@gmail.com>
2014-10-15 18:15:39 -0400

    base views and route

--- api/app.js
+++ api/app.js
@@ -1,15 +1,37 @@
-{
-  "name": "DumpCheckerApi",
-  "version": "0.0.0",
-  "description": "API for DumpChecker",
-  "main": "app.js",
-  "author": {
-    "name": "William Buchwalter",
-    "email": "wbuchwalter@gmail.com"
-  },
-  "dependencies": {
-    "express": "3.4.4",
-    "jade": "*",
-    "stylus": "*"
-  }
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
 }
+
+app.get('/', routes.index);
+app.get('/check', test.find);
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
